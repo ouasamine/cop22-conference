db493924f092b1d1f1039e0b6e600a094dc3a59a
Amine <ouas.amine@outlook.com>
2022-09-14 14:34:29 +0100

    Fix Linter Errors

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,6 @@
 const hamburgerBtn = document.querySelector('#hamburger-button');
 const mobileMenu = document.querySelector('#mobile-menu');
-const mobileLnk = mobileMenu.querySelectorAll("li a");
+const mobileLnk = mobileMenu.querySelectorAll('li a');
 hamburgerBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('hide-all');
   hamburgerBtn.classList.toggle('close-button');
--- js/speakers.js
+++ js/speakers.js
@@ -1,60 +1,60 @@
-const speaker_1 = {
-  name: "H.E. Dr. Hakima El Haite",
-  position: "Minister Delegate in Charge of Environment of the Minister of Energy, Mining, Water and Environment of Morocco",
-  desc: "PHD in Environmental Engineering from the Scool of Mines of Saint Etienne and a Degree in political communication, University of Washington.",
-  img: "./img/hakima-el-haite-900x600.jpg"
+const speaker1 = {
+  name: 'H.E. Dr. Hakima El Haite',
+  position: 'Minister Delegate in Charge of Environment of the Minister of Energy, Mining, Water and Environment of Morocco',
+  desc: 'PHD in Environmental Engineering from the Scool of Mines of Saint Etienne and a Degree in political communication, University of Washington.',
+  img: './img/hakima-el-haite-900x600.jpg',
 };
-const speaker_2 = {
-  name: "H. E. Siti Nurbaya Bakar",
-  position: "Minister of Environment and Forestry, Republic of Indonesia",
+const speaker2 = {
+  name: 'H. E. Siti Nurbaya Bakar',
+  position: 'Minister of Environment and Forestry, Republic of Indonesia',
   desc: "Master's degree at the International Institute for Aerospace Survey and Earth Sciences, Netherlands and Ph.D. from the Bogor Institute of Agriculture.",
-  img: "./img/siti-nurbaya-900x600.jpg"
+  img: './img/siti-nurbaya-900x600.jpg',
 };
-const speaker_3 = {
-  name: "Erik Solheim",
-  position: "Executive Director of United Nations Environment Programme-UNEP",
-  desc: "Graduated from the University of Oslo in 1980 with a cand.mag. degree after studying history, sociology and political science.",
-  img: "./img/erik-solheim-900x600.jpg"
-};
-
-const speaker_4 = {
-  name: "Mafalda Duarte",
-  position: "CEO of Climate Investment Funds (CIF)",
-  desc: "Master’s in Economic Policy Management, Development Economics and International Development, Columbia University.",
-  img: "./img/mafalda-duarte-900x600.jpg"
+const speaker3 = {
+  name: 'Erik Solheim',
+  position: 'Executive Director of United Nations Environment Programme-UNEP',
+  desc: 'Graduated from the University of Oslo in 1980 with a cand.mag. degree after studying history, sociology and political science.',
+  img: './img/erik-solheim-900x600.jpg',
 };
 
-const speaker_5 = {
-  name: "Bea Perez",
-  position: "Head of Global Office of Sustainability",
-  desc: "Bea holds a Bachelor of Science degree in Marketing from the University of Maryland.",
-  img: "./img/bea-perez-900x600.jpg"
+const speaker4 = {
+  name: 'Mafalda Duarte',
+  position: 'CEO of Climate Investment Funds (CIF)',
+  desc: 'Master’s in Economic Policy Management, Development Economics and International Development, Columbia University.',
+  img: './img/mafalda-duarte-900x600.jpg',
 };
 
-const speaker_6 = {
-  name: "Lisa Walker",
-  position: "CEO of Ecosphere +",
-  desc: " Lisa worked in the oil and gas sector for >17 years and has been closely involved in all aspects of climate change strategy development & implementation.",
-  img: "./img/lisa-walker-900x600.jpg"
+const speaker5 = {
+  name: 'Bea Perez',
+  position: 'Head of Global Office of Sustainability',
+  desc: 'Bea holds a Bachelor of Science degree in Marketing from the University of Maryland.',
+  img: './img/bea-perez-900x600.jpg',
 };
 
-const speaker_7 = {
-  name: "Lisa Davis",
-  position: "Siemens Oil & Gas Responsible",
-  desc: "Bachelor of Science (B.S.), Chemical Engineering, University of California.",
-  img: "./img/lisa-davis-900x600.jpg"
+const speaker6 = {
+  name: 'Lisa Walker',
+  position: 'CEO of Ecosphere +',
+  desc: ' Lisa worked in the oil and gas sector for >17 years and has been closely involved in all aspects of climate change strategy development & implementation.',
+  img: './img/lisa-walker-900x600.jpg',
 };
 
-const speaker_8 = {
-  name: "M. Mustapha Bakkoury",
-  position: "Chairman of the Management Board of Masen and President of the Casablanca-Settat Region, Morocco",
-  desc: "Master’s degree in banking and finance, Ecole des Ponts et Chaussées, Paris.",
-  img: "./img/mustapha-bakkoury-900x600.png"
+const speaker7 = {
+  name: 'Lisa Davis',
+  position: 'Siemens Oil & Gas Responsible',
+  desc: 'Bachelor of Science (B.S.), Chemical Engineering, University of California.',
+  img: './img/lisa-davis-900x600.jpg',
 };
 
-const speakers = [speaker_1,speaker_2,speaker_3,speaker_4,speaker_5,speaker_6,
-  speaker_7,speaker_8];
-const speakersContainer = document.querySelector("#speakers > div");
+const speaker8 = {
+  name: 'M. Mustapha Bakkoury',
+  position: 'Chairman of the Management Board of Masen and President of the Casablanca-Settat Region, Morocco',
+  desc: 'Master’s degree in banking and finance, Ecole des Ponts et Chaussées, Paris.',
+  img: './img/mustapha-bakkoury-900x600.png',
+};
+
+const speakers = [speaker1, speaker2, speaker3, speaker4, speaker5, speaker6,
+  speaker7, speaker8];
+const speakersContainer = document.querySelector('#speakers > div');
 
 speakers.forEach((speaker) => {
   speakersContainer.innerHTML += `
